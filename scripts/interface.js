8e8b5db05eafc56c295c531fd646818ba4a9513b
kevinCaldieraro <kevincaldieraro123@gmail.com>
2022-06-14 12:35:49 -0300

    update files

---
+++ scripts/interface.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.addEventListener('click', handleClick);
+  });
+});
+
+function handleClick(event) {
+  let position = event.target.id;
+  if (handleMove(position)) {
+    setTimeout(() => {
+      alert('end');
+    }, 10);
+  }
+  updateSquare(position);
+}
+
+function updateSquare(position) {
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class='${symbol}'></div>`;
+}
+
+// function updateSquares() {
+//   let squares = document.querySelectorAll('.square');
+//   squares.forEach(square => {
+//     let position = square.id;
+//     let symbol = board[position];
+
+//     if (symbol != '') {
+//       square.innerHTML = `<div class='${symbol}'></div>`;
+//     }
+//   });
+// }
